65873d551e0e1ae43fc0b8620aec37ff70a137ca
lc-masa <myamanakalife6911@gmail.com>
2023-04-11 17:48:18 +0900

    fixed Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,15 @@
+import React from "react";
 import Navbar from "./Navbar";
 
-const Layout = ({ children }) => {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Navbar />
-      <div>{children}</div>
+      {children}
     </>
   );
 };
