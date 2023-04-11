4c89952e684a735b05c9f142df8771b5e2ede3a0
lc-masa <myamanakalife6911@gmail.com>
2023-04-11 17:50:34 +0900

    added /coins link to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,6 +9,7 @@ return (
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/profile">Profile</Link>
+      <Link href="/coins">Coins</Link>
     </div>
   </div>
 )
